2b3fa584b9b02080e86fd07543bba23d5d1f0be2
Jamil Umar <jamil.umarmuhammad@gmail.com>
2021-06-16 19:16:45 +0100

    init commit

---
+++ variable.js
@@ -0,0 +1,7 @@
+// Wnat are variables ?
+// They are names you give to a data so you can use them later.
+// You declare variables using var, let or const.
+
+const myName = 'Mohammad Jamil Umar';
+let myNum = 100;
+var isRight = true;
